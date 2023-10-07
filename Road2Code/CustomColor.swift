0117abb1d5b3863cafd1539f55c9461a7caad515
Amine <117638885+BenMousaAmine@users.noreply.github.com>
2023-10-07 18:10:16 +0200

    Create CustomColor.swift

---
+++ Road2Code/CustomColor.swift
@@ -0,0 +1,19 @@
+//
+//  CustomColor.swift
+//  Road2Code
+//
+//  Created by user245218 on 10/7/23.
+//
+
+import SwiftUI
+
+extension Color {
+   static let CostumColor = Color (
+        red: 61 / 255.0 ,
+        green:   59 / 255.0 ,
+        blue: 142 / 255.0 )
+    
+    
+}
+
+
