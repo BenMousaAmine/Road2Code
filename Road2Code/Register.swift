c6df58e435f6b44c7e65c280d1cbff89b7e99118
Amine <117638885+BenMousaAmine@users.noreply.github.com>
2023-10-07 18:27:48 +0200

    Update Register.swift

--- Road2Code/Register.swift
+++ Road2Code/Register.swift
@@ -10,118 +10,74 @@ import SwiftUI
 struct Register: View {
     @State var nickname = ""
     @State var password = ""
+    @State var repeatPassword = ""
+    @State var passVisb : Bool = false
+    @State var repeatPassVisb : Bool = false
     @State var email = ""
+ 
     @State var itemName: String = ""
     @State var isOn = false
-    @State var borderColor: Bool = false
-    let colorPerso = Color(
-        red: 61 / 255.0 ,
-        green:   59 / 255.0 ,
-        blue: 142 / 255.0 )
-    
-    
-    
-    struct TextInputField: View {
-        let placeHolder: String
-        @Binding var textValue: String
-        @State private var larger = true
-        var body: some View {
-            ZStack(alignment: .leading) {
-                Text(placeHolder)
-                    .foregroundColor(Color(.placeholderText))
-                    .offset(y: textValue.isEmpty ? 0 : -25)
-                    .scaleEffect(textValue.isEmpty ? 1: 0.8, anchor: .leading)
-                TextField("", text: $textValue)
-            }
-            .padding(.top, textValue.isEmpty ? 0 : 15)
-            .frame(height: 52)
-            .padding(.horizontal, 16)
-            .overlay(RoundedRectangle(cornerRadius: 12).stroke(lineWidth: 1).foregroundColor(.gray))
-            .animation(.default , value: larger )
-            .onAppear{larger = false}
-            
-        }
-    }
+  
     
     var body: some View {
         NavigationStack {
             VStack {
-                TextInputField(placeHolder: "nick name" , textValue: $itemName)
-                Image("Road2Code")
-                    .resizable().frame(width: 80, height: 80)
-                    .cornerRadius(10)
-                Text("ROAD2CODE")
-                    .font(.largeTitle)
-                    .fontWeight(.heavy)
-                    .foregroundColor(colorPerso)
-                if borderColor == false {
-                    TextField("Nickname", text: $nickname)
-                        .textFieldStyle(.roundedBorder)
-                        .frame(width: 250, height: 40)
-                        .border(colorPerso)
-                        .cornerRadius(5)
-                        .padding(15)
-                    TextField("Email", text: $email)
-                        .textFieldStyle(.roundedBorder)
-                        .cornerRadius(10)
-                        .frame(width: 250, height: 40)
-                        .border(colorPerso)
-                        .padding(15)
-                    SecureField("Password", text: $password)
-                        .textFieldStyle(.roundedBorder)
-                        .cornerRadius(10)
-                        .frame(width: 250, height: 40)
-                        .border(colorPerso)
-                        .padding(15)
+              Text("Register")
+                CustomTextField(text:$nickname , imageName: "person" , placeholder: "Nikname*")
+                    .frame(width: 300)
+                CustomTextField(text:$nickname , imageName: "mail" , placeholder: "Email*")
+                    .frame(width: 300)
+                CustomPassword(password: $password, isPasswordVisible : $passVisb ,placeholder: "password")
+                    .frame(width: 300)
+                CustomPassword(password: $repeatPassword, isPasswordVisible : $repeatPassVisb ,placeholder:"Repeat your password ")
+                    .frame(width: 300)
+            HStack() {
+                    Toggle(isOn: $isOn) {}
+                        .toggleStyle(iOSCheckboxToggleStyle())
+                        .foregroundColor(Color.CostumColor)
+                    Text("Accetta i termini di servizio")
+                        .foregroundColor(Color.CostumColor)
                 }
-                else {
-                    TextField("Nickname", text: $nickname)
-                        .textFieldStyle(.roundedBorder)
-                        .frame(width: 250, height: 40)
-                        .border(Color.red)
-                        .cornerRadius(5)
-                        .padding(15)
-                    TextField("Email", text: $email)
-                        .textFieldStyle(.roundedBorder)
-                        .cornerRadius(10)
-                        .frame(width: 250, height: 40)
-                        .border(Color.red)
-                        .padding(15)
-                    SecureField("Password", text: $password)
-                        .textFieldStyle(.roundedBorder)
-                        .cornerRadius(10)
-                        .frame(width: 250, height: 40)
-                        .border(Color.red)
-                        .padding(15)
-                }
-                HStack {
-                    Toggle(isOn: $isOn) {
-                        Text("Accetta i termini di servizio")
-                    }
-                }
+                
+                
+                
+                
                 Button(action: {
-                    if nickname == "" && email == "" && password == "" {
-                        borderColor = true
-                    }
+                    
                 }) {
                     Text("Registrati")
                         .frame(width: 250, height: 45)
                         .font(.system(size: 26))
-                        .foregroundColor(colorPerso)
+                        .foregroundColor(Color.CostumColor)
                         .padding(5)
                 }.background(Color.white)
                     .cornerRadius(10)
-                    .border(colorPerso)
+                    .border(Color.CostumColor)
                     .disabled(nickname != "" && password != "" && email != "" ? true : false)
                 HStack {
                     Text("Hai già un account?")
                     NavigationLink(destination: Login()) {
                         Text("Accedi")
-                    }.foregroundColor(colorPerso)
-                        .navigationBarBackButtonHidden(true)
+                    }
+                    .foregroundColor(Color.CostumColor)
+                    .navigationBarBackButtonHidden(true)
+
                 }
             }
         }
+    }
+    
+}
+struct iOSCheckboxToggleStyle: ToggleStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        Button(action: {
+            configuration.isOn.toggle()
+        }, label: {
+            HStack {
+                Image(systemName: configuration.isOn ? "checkmark.square" : "square")
+                configuration.label
+            }
+        })
     }
 }
 
