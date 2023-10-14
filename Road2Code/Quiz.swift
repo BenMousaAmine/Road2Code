d4f45c0b36ebc15669333d92f40819dfa8056cf4
Amine <117638885+BenMousaAmine@users.noreply.github.com>
2023-10-14 15:30:05 +0200

    Create Quiz.swift

---
+++ Road2Code/Quiz.swift
@@ -0,0 +1,20 @@
+//
+//  Quiz.swift
+//  Road2Code
+//
+//  Created by user245218 on 10/14/23.
+//
+
+import SwiftUI
+
+struct Quiz: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Quiz_Previews: PreviewProvider {
+    static var previews: some View {
+        Quiz()
+    }
+}
