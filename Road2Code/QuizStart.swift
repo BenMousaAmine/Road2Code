c9195bc5d1248caa1d45edd8ff1a141b21ef2db7
Amine <117638885+BenMousaAmine@users.noreply.github.com>
2023-10-17 16:33:51 +0200

    Update QuizStart.swift

--- Road2Code/QuizStart.swift
+++ Road2Code/QuizStart.swift
@@ -6,38 +6,68 @@
 //
 
 import SwiftUI
+import Supabase
+
+struct QuizInfo: Codable , Identifiable  {
+    let id = UUID()
+    var tempo: String
+}
+class Api : ObservableObject {
+    @Published var quiz = ErrorMessage()
+    
+    func loadData (completion : @escaping (ErrorMessage)->()){
+        guard let url = URL(string: "https://ywsbqtupbreoiylewpxr.supabase.co/rest/v1/Quiz?select=Tempo")else {
+            print ("invalid url ")
+            return ;
+        }
+        URLSession.shared.dataTask(with: url ){data ,response , error in
+            let quiz = try! JSONDecoder().decode(ErrorMessage.self , from: data!)
+            DispatchQueue.main.async{
+                completion(quiz)
+            }
+        }.resume()
+    }
+}
 
 
 struct QuizStart: View {
     let language: String
-
+    @State private var Argomenti: String = ""
+    @State private var tempo: String  = ""
+    @State var quizs = [QuizInfo]()
+    
+    
     var body: some View {
-        VStack{
-            VStack {
-                Image(language)
-                    .resizable()
-                    .frame(width: 200, height: 120, alignment: .bottom)
+        
+        List(quizs){ quiz in
+            VStack{
+                VStack {
+                    Image(language)
+                        .resizable()
+                        .frame(width: 200, height: 120, alignment: .bottom)
+                }
+                VStack{
+                    Text(language)
+                        .font(.largeTitle)
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color.CustomColor)
+                        .bold()
+                        .padding()
+                    Text("Argomenti: \(Argomenti)")
+                        .font(.title)
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color.CustomColor)
+                    Text("TEMPO: \(quiz.tempo)")
+                        .font(.title)
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color.CustomColor)
+                }
+                .frame(width: 350.0, height: 400.0)
+                .border(Color.CustomColor, width: 2)
+                .padding(10)
                 
             }
-            VStack{
-                Text(language)
-                    .font(.largeTitle)
-                    .fontWeight(.heavy)
-                    .foregroundColor(Color.CustomColor)
-                    .bold()
-                    .padding()
-                Text("Argomenti : ")
-                    .font(.title)
-                    .fontWeight(.heavy)
-                    .foregroundColor(Color.CustomColor)
-                Text("TEMPO")
-                    .font(.title)
-                    .fontWeight(.heavy)
-                    .foregroundColor(Color.CustomColor)
-            }
-            .frame(width: 350.0, height: 400.0)
-            .border(Color.CustomColor ,width:2)
-            .padding(10)
+        }
             HStack {
                 NavigationLink(destination: Quiz()) {
                     Text("Inizia quiz")
@@ -48,11 +78,11 @@ struct QuizStart: View {
                 }.background(Color.CustomColor)
                     .cornerRadius(10)
                     .padding(10)
-                    .navigationBarBackButtonHidden(true)
             }
         }
     }
-}
+
+
 
 struct QuizStart_Previews: PreviewProvider {
     static var previews: some View {
