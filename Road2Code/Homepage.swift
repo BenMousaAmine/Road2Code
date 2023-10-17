f4d91e756de68735934ff95e400760ae1ea9fa28
Amine <117638885+BenMousaAmine@users.noreply.github.com>
2023-10-17 16:51:23 +0200

    Update Homepage.swift

--- Road2Code/Homepage.swift
+++ Road2Code/Homepage.swift
@@ -7,68 +7,86 @@
 
 import SwiftUI
 
-struct LanguageButtonView: View {
-    let language: String
-
-    var body: some View {
-        HStack {
-            Image(language)
-                .resizable()
-                .frame(width: 130, height: 80, alignment: .bottom)
-            Text(language)
-                .padding(10)
-                .fontWeight(.bold)
-                .foregroundColor(Color.CustomColor)
-        }
-    }
-}
 struct Homepage: View {
-    @Binding var nickname: String
-    @State private var selectedLanguage: String?
-    let languages = ["Java", "JavaScript", "Python", "React", "Database", "HTML:CSS"]
-
+    let colorPerso = Color(
+        red: 61 / 255.0 ,
+        green:   59 / 255.0 ,
+        blue: 142 / 255.0 )
     var body: some View {
-        NavigationView {
+        VStack {
+            Image("Road2Code")
+                .resizable().frame(width: 50, height: 50, alignment: .leading)
+                .cornerRadius(10)
+            Text("Ciao Nickname")
+                .font(.system(size: 36))
+                .bold()
+                .foregroundColor(colorPerso)
+            Spacer().frame(height: 80)
+            Text("Completa i seguenti quiz e ")
+                .font(.system(size: 18))
+                .bold()
+                .padding(7)
+            Text("Ottieni la tua roadmap")
+                .foregroundColor(colorPerso)
+                .bold()
+                .font(.system(size: 26))
+            Spacer()
             VStack {
-                Image("Road2Code")
-                    .resizable()
-                    .frame(width: 50, height: 50, alignment: .leading)
-                    .cornerRadius(10)
-                Text("Ciao " + nickname)
-                    .font(.system(size: 36))
-                    .bold()
-                    .foregroundColor(Color.CustomColor)
-                Spacer().frame(height: 20)
-                Text("Completa i seguenti quiz e ")
-                    .font(.system(size: 18))
-                    .bold()
-                    .padding(7)
-                Text("Ottieni la tua roadmap")
-                    .foregroundColor(Color.CustomColor)
-                    .bold()
-                    .font(.system(size: 26))
-                Spacer()
-                VStack {
-                    ForEach(languages, id: \.self) { language in
-                        NavigationLink(destination: QuizStart(language: language), tag: language, selection: $selectedLanguage) {
-                                LanguageButtonView(language: language)
-                            }
-                        .buttonStyle(PlainButtonStyle())
-                        .onTapGesture {
-                            selectedLanguage = language
-                        }
+                HStack {
+                    VStack {
+                        Image("Java")
+                            .resizable().frame(width: 150, height: 80, alignment: .bottom)
+                        Text("Java")
+                            .padding(10)
+                            .foregroundColor(colorPerso)
+                    }
+                    VStack {
+                        Image("JavaScript")
+                            .resizable().frame(width: 150, height: 80, alignment: .bottom)
+                        Text("JavaScript")
+                            .padding(10)
+                            .foregroundColor(colorPerso)
                     }
                 }
+                HStack {
+                    VStack {
+                        Image("Python")
+                            .resizable().frame(width: 150, height: 80, alignment: .bottom)
+                        Text("Python")
+                            .padding(10)
+                            .foregroundColor(colorPerso)
+                    }
+                    VStack {
+                        Image("React")
+                            .resizable().frame(width: 150, height: 80, alignment: .bottom)
+                        Text("React")
+                            .padding(10)
+                            .foregroundColor(colorPerso)
+                    }
+                }.padding(20)
+                HStack {
+                    VStack {
+                        Image("Database")
+                            .resizable().frame(width: 150, height: 80, alignment: .bottom)
+                        Text("Database")
+                            .padding(10)
+                            .foregroundColor(colorPerso)
+                    }
+                    VStack {
+                        Image("HTML:CSS")
+                            .resizable().frame(width: 150, height: 80, alignment: .bottom)
+                        Text("HTML/CSS")
+                            .padding(10)
+                            .foregroundColor(colorPerso)
+                    }
+                }.padding(30)
             }
-         
         }
     }
 }
 
-
 struct Homepage_Previews: PreviewProvider {
-    @State static var sampleNickname: String = "Prova"
     static var previews: some View {
-        Homepage(nickname: $sampleNickname)
+        Homepage()
     }
 }
