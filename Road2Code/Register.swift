ec8a1adfed7a5c98514c2cd97aa1024ea634e1b2
Amine <117638885+BenMousaAmine@users.noreply.github.com>
2023-10-17 16:50:03 +0200

    Update Register.swift

--- Road2Code/Register.swift
+++ Road2Code/Register.swift
@@ -10,106 +10,144 @@ import SwiftUI
 struct Register: View {
     @State var nickname = ""
     @State var password = ""
-    @State var repeatPassword = ""
-    @State var passVisb : Bool = false
-    @State var repeatPassVisb : Bool = false
     @State var email = ""
-    @State var date = ""
     @State var itemName: String = ""
     @State var isOn = false
-    @State var isLogged = false
+    @State var borderColor: Bool = false
+    @State var eyeClick : Bool = false
+    let colorPerso = Color(
+        red: 61 / 255.0 ,
+        green:   59 / 255.0 ,
+        blue: 142 / 255.0 )
     
-    func insertData(nickname: String, email: String, password: String, notifica: Bool) {
-        let supabaseURL = URL(string: "https://ywsbqtupbreoiylewpxr.supabase.co/rest/v1/Utente")
-        var request = URLRequest(url: supabaseURL!)
-        
-        request.httpMethod = "POST"
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.setValue("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl3c2JxdHVwYnJlb2l5bGV3cHhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY0MDYzMjcsImV4cCI6MjAxMTk4MjMyN30.r3IN1U6HcBefc8ZJx0STaY0-aceQ8UQS7-omqdiGnhw", forHTTPHeaderField: "apikey")
-        
-        let data: [String: Any] = [
-            "Immagine": "https://ibb.co/qjgHyTG",
-            "Nickname": nickname,
-            "Email": email,
-            "Password": password,
-            "Notifica": notifica
-        ]
-        
-        do {
-            let jsonData = try JSONSerialization.data(withJSONObject: data)
-            request.httpBody = jsonData
-        } catch {
-            print("Error serializing JSON data: \(error)")
-            return
-        }
-        
-        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-            if let error = error {
-                print("Error: \(error)")
-            } else if let data = data {
-                if let responseString = String(data: data, encoding: .utf8) {
-                    print("Response: \(responseString)")
-                    isLogged = true
-                    // Data inserted successfully
-                }
+    
+    
+    struct TextInputField: View {
+        let placeHolder: String
+        @Binding var textValue: String
+        @State private var larger = true
+        var body: some View {
+            ZStack(alignment: .leading) {
+                Text(placeHolder)
+                    .foregroundColor(Color(.placeholderText))
+                    .offset(y: textValue.isEmpty ? 0 : -25)
+                    .scaleEffect(textValue.isEmpty ? 1: 0.8, anchor: .leading)
+                TextField("", text: $textValue)
             }
+            .padding(.top, textValue.isEmpty ? 0 : 15)
+            .frame(height: 52)
+            .padding(.horizontal, 16)
+            
+            .overlay(RoundedRectangle(cornerRadius: 8).stroke(lineWidth: 1).foregroundColor(.gray))
+            
+            
         }
-        
-        task.resume()
     }
     
     var body: some View {
-        if isLogged {
-            Tab(nickname: $nickname)
-        }
-        else {
-            NavigationStack {
-                VStack {
-                    Image("Road2Code")
-                        .resizable().frame(width: 80, height: 80)
+        NavigationStack {
+            VStack {
+                TextInputField(placeHolder: "nick name" , textValue: $itemName)
+                Image("Road2Code")
+                    .resizable().frame(width: 80, height: 80)
+                    .cornerRadius(10)
+                Text("ROAD2CODE")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .foregroundColor(colorPerso)
+                if borderColor == false {
+                    TextField("Nickname", text: $nickname)
+                        .textFieldStyle(.roundedBorder)
+                        .frame(width: 250, height: 40)
+                        .border(colorPerso)
+                        .cornerRadius(5)
+                        .padding(15)
+                    TextField("Email", text: $email)
+                        .textFieldStyle(.roundedBorder)
                         .cornerRadius(10)
-                    Text("ROAD2CODE")
-                        .font(.largeTitle)
-                        .fontWeight(.heavy)
-                        .foregroundColor(Color.CustomColor)
-                    CustomTextField(text:$nickname , imageName: "person" , placeholder: "Nickname")
-                        .frame(width: 300)
-                    CustomTextField(text:$email , imageName: "mail" , placeholder: "Email")
-                        .frame(width: 300)
-                    CustomPassword(password: $password, isPasswordVisible : $passVisb ,placeholder: "Password")
-                        .frame(width: 300)
-                    CustomPassword(password: $repeatPassword, isPasswordVisible : $repeatPassVisb ,placeholder:"Ripeti password ")
-                        .frame(width: 300)
+                        .frame(width: 250, height: 40)
+                        .border(colorPerso)
+                        .padding(15)
                     
-                    HStack() {
-                        Toggle(isOn: $isOn) {}
-                            .toggleStyle(iOSCheckboxToggleStyle())
-                            .foregroundColor(Color.CustomColor)
-                        Text("Accetta i termini di servizio")
-                            .foregroundColor(Color.CustomColor)
-                    }.padding(20)
-                    /*Button(action: {
-                        insertData(nickname: nickname, email: email, password: password, notifica: isOn)
-                    }) {*/
-                    NavigationLink(destination: Tab(nickname: $nickname)){
-                        Text("Registrati")
-                            .frame(width: 250, height: 45)
-                            .font(.system(size: 26))
-                            .foregroundColor(Color.CustomColor)
-                            .padding(5)
-                    }.background(Color.white)
-                        .cornerRadius(10)
-                        .border(Color.CustomColor)
-                        .disabled(nickname != "" && password != "" && email != "" ? true : false)
-                    HStack {
-                        Text("Hai già un account?")
-                        NavigationLink(destination: Login()) {
-                            Text("Accedi")
+                    ZStack(alignment: .trailing){
+                        if !eyeClick {
+                            SecureField("Password", text: $password)
+                                .textFieldStyle(.roundedBorder)
+                                .cornerRadius(10)
+                                .frame(width: 250, height: 40)
+                                .border(colorPerso)
+                                .padding(15)
+                        }else{
+                            TextField("Password", text: $password)
+                                .textFieldStyle(.roundedBorder)
+                                .cornerRadius(10)
+                                .frame(width: 250, height: 40)
+                                .border(colorPerso)
+                                .padding(15)
                         }
-                        .foregroundColor(Color.CustomColor)
-                        .navigationBarBackButtonHidden(true)
+                        Button(action: {
+                            eyeClick = !eyeClick
+                        }, label: {
+                            Image(systemName: !eyeClick ? "eye" : "eye.slash" )
+                                .foregroundColor(.gray)
+                                .padding()
+                        })
                         
                     }
+                    .padding(.horizontal, 20)
+                    .animation(.easeInOut(duration: 0.3), value: eyeClick)
+                }
+                else {
+                    TextField("Nickname", text: $nickname)
+                        .textFieldStyle(.roundedBorder)
+                        .frame(width: 250, height: 40)
+                        .border(Color.red)
+                        .cornerRadius(5)
+                        .padding(15)
+                    TextField("Email", text: $email)
+                        .textFieldStyle(.roundedBorder)
+                        .cornerRadius(10)
+                        .frame(width: 250, height: 40)
+                        .border(Color.red)
+                        .padding(15)
+                    SecureField("Password", text: $password)
+                        .textFieldStyle(.roundedBorder)
+                        .cornerRadius(10)
+                        .frame(width: 250, height: 40)
+                        .border(Color.red)
+                        .padding(15)
+                }
+                HStack() {
+                    Toggle(isOn: $isOn) {}
+                        .toggleStyle(iOSCheckboxToggleStyle())
+                        .foregroundColor(colorPerso)
+                    Text("Accetta i termini di servizio")
+                        .foregroundColor(colorPerso)
+                }
+                
+                
+                
+                
+                Button(action: {
+                    if nickname == "" && email == "" && password == "" {
+                        borderColor = true
+                    }
+                }) {
+                    Text("Registrati")
+                        .frame(width: 250, height: 45)
+                        .font(.system(size: 26))
+                        .foregroundColor(colorPerso)
+                        .padding(5)
+                }.background(Color.white)
+                    .cornerRadius(10)
+                    .border(colorPerso)
+                    .disabled(nickname != "" && password != "" && email != "" ? true : false)
+                HStack {
+                    Text("Hai già un account?")
+                    NavigationLink(destination: Login()) {
+                        Text("Accedi")
+                    }.foregroundColor(colorPerso)
+                        .navigationBarBackButtonHidden(true)
                 }
             }
         }
