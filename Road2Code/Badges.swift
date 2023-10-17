aade0c986306945b87b9eddf0aa580f860204fb8
Amine <117638885+BenMousaAmine@users.noreply.github.com>
2023-10-17 17:14:27 +0200

    Update Badges.swift

--- Road2Code/Badges.swift
+++ Road2Code/Badges.swift
@@ -4,15 +4,31 @@
 //
 //  Created by user245218 on 10/5/23.
 //
-
 import SwiftUI
 
 struct Badges: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Collezione Medaglie")
+                .font(.system(size: 30))
+                .fontWeight(.heavy)
+                .foregroundColor(Color.CustomColor)
+            Spacer().frame(height: 30)
+            Text("Non hai medaglie attualmente")
+            // Stile medaglie
+            /*HStack {
+                Image(systemName: "bell")
+                    .foregroundColor(Color.white)
+                Text("Java")
+                    .foregroundColor(Color.white)
+                    .frame(width: 170, height: 100)
+                    .fontWeight(.bold)
+            }.background(Color.CustomColor)
+                .cornerRadius(10)*/
+        }
+        
     }
 }
-
 struct Badges_Previews: PreviewProvider {
     static var previews: some View {
         Badges()
